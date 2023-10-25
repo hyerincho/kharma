85efb2c8a532e49486174117085eb1f00f254874
Hyerin Cho <chyerin1996@gmail.com>
2023-10-25 09:18:35 -0400

    updates for the wks test scripts

--- tests/bondi/run.sh
+++ tests/bondi/run.sh
@@ -10,12 +10,14 @@ conv_2d() {
     do
       # Four blocks
       half=$(( $res / 2 ))
-      $BASE/run.sh -i $BASE/pars/bondi.par parthenon/output0/dt=1000 debug/verbose=1 \
+      $BASE/run.sh -b ../kharma_wks_ratio2.cuda -i $BASE/pars/bondi.par parthenon/output0/dt=1000 parthenon/output1/dt=1000 debug/verbose=1 \
                                            parthenon/mesh/nx1=$res parthenon/mesh/nx2=$res parthenon/mesh/nx3=1 \
-                                           parthenon/meshblock/nx1=$half parthenon/meshblock/nx2=$half parthenon/meshblock/nx3=1 \
+                                           parthenon/meshblock/nx1=$half parthenon/meshblock/nx2=$half parthenon/meshblock/nx3=1 coordinates/lin_frac=0.75 GRMHD/reconstruction=weno5 \
                                            $2 >log_${1}_${res}.txt 2>&1
         mv bondi.out0.00000.phdf bondi_2d_${res}_start_${1}.phdf
         mv bondi.out0.final.phdf bondi_2d_${res}_end_${1}.phdf
+        mv bondi.out1.00000.rhdf bondi_2d_${res}_start_${1}.rhdf
+        mv bondi.out1.final.rhdf bondi_2d_${res}_end_${1}.rhdf
     done
     check_code=0
     python check.py $ALL_RES "$3" $1 || check_code=$?
@@ -28,14 +30,15 @@ conv_2d() {
 }
 
 # Test coordinates (raw ks?)
-conv_2d fmks coordinates/transform=fmks "in 2D, FMKS coordinates"
-conv_2d mks coordinates/transform=mks "in 2D, MKS coordinates"
+conv_2d wks coordinates/transform=wks "in 2D, WKS coordinates"
+#conv_2d fmks coordinates/transform=fmks "in 2D, FMKS coordinates" 
+#conv_2d mks coordinates/transform=mks "in 2D, MKS coordinates"
 # TODO fix this: converges at 2.3!!
 #conv_2d eks coordinates/transform=eks "in 2D, EKS coordinates"
 
 # Recon
-conv_2d linear_mc GRMHD/reconstruction=linear_mc "in 2D, linear recon with MC limiter"
-conv_2d linear_vl GRMHD/reconstruction=linear_vl "in 2D, linear recon with VL limiter"
+#conv_2d linear_mc GRMHD/reconstruction=linear_mc "in 2D, linear recon with MC limiter"
+#conv_2d linear_vl GRMHD/reconstruction=linear_vl "in 2D, linear recon with VL limiter"
 
 # And the GRIM/classic driver
 # TODO these crash, likely an implicit w/o B field thing
