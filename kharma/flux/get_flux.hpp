fb4abcf4c651f3f573fda7d5fb55718c61b290d6
Hyerin Cho <chyerin1996@gmail.com>
2024-04-10 16:30:24 -0400

    last fix

--- kharma/flux/get_flux.hpp
+++ kharma/flux/get_flux.hpp
@@ -163,8 +163,8 @@ inline TaskStatus GetFlux(MeshData<Real> *md)
             // We template on reconstruction type to avoid a big switch statement here.
             // Instead, a version of GetFlux() is generated separately for each reconstruction/direction pair.
             // See reconstruction.hpp for all the implementations.
-            if (ismr_poles) KReconstruction::reconstruct<Recon, dir>(member, P_all(bl), k, j, b.is, b.ie, Pl_s, Pr_s, ismr_nlevels, ng);
-            else KReconstruction::reconstruct<Recon, dir>(member, P_all(bl), k, j, b.is, b.ie, Pl_s, Pr_s);
+            if (ismr_poles) KReconstruction::ReconstructRow<Recon, dir>(member, P_all(bl), k, j, b.is, b.ie, Pl_s, Pr_s, ismr_nlevels, ng);
+            else KReconstruction::ReconstructRow<Recon, dir>(member, P_all(bl), k, j, b.is, b.ie, Pl_s, Pr_s);
 
             // Sync all threads in the team so that scratch memory is consistent
             member.team_barrier();
